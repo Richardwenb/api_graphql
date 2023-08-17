3fddacfad8a8a4a3e4732ebc63138aedbbce3ca7
Richard Wendel <49850416+Richardwenb@users.noreply.github.com>
2023-08-17 20:18:14 -0300

    atualizacao

--- schema.py
+++ schema.py
@@ -1,15 +1,18 @@
 import graphene
-from graphene import ObjectType, String
-from graphene import Schema
-from flask_graphql import GraphQLView
-from app import *
 
+class News(graphene.ObjectType):
+    title = graphene.String()
+    content = graphene.String()
 
 class Query(graphene.ObjectType):
-    noticia = graphene.String()
+    news = graphene.List(News)
 
-    def resolve_noticia(self, info):
-        return "Bolsa de valores aumenta em 30%"
+    def resolve_news(self, info):
+        # Substitua esta lista com suas notícias reais
+        fake_news = [
+            {"title": "Santos vence a partida", "content": "O time Santos venceu por 2-0."},
+            {"title": "Nova contratação para o Santos", "content": "Santos contrata novo jogador."},
+        ]
+        return [News(title=news["title"], content=news["content"]) for news in fake_news]
 
-    schema = Schema(query=Query)
 schema = graphene.Schema(query=Query)
