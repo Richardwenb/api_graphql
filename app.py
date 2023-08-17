26bc7c1db9c39f2c4e6ea7b0df246700b985dcb5
Richard Wendel <49850416+Richardwenb@users.noreply.github.com>
2023-08-17 20:17:45 -0300

    atualizacao

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from graphene import ObjectType, String, List, Schema
+from flask_graphql import GraphQLView
+
 app = Flask(__name__)
 
-from schema import *
+class News(ObjectType):
+    title = String()
+    content = String()
+
+class Query(ObjectType):
+    news = List(News)
+
+    def resolve_news(self, info):
+        # Substitua esta lista com suas notícias reais
+        fake_news = [
+            {"title": "Santos vence a partida", "content": "O time Santos venceu por 2-0."},
+            {"title": "Nova contratação para o Santos", "content": "Santos contrata novo jogador."},
+        ]
+        return [News(title=news["title"], content=news["content"]) for news in fake_news]
+
+schema = Schema(query=Query)
+
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
